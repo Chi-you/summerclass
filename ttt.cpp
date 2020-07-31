16b50deec9f73f6cd9da61bf18ea7d7e04ec6950
Chi-you <yoyolee789@gmail.com>
2020-07-31 22:26:56 +0800

    change name

--- ttt.cpp
+++ ttt.cpp
@@ -3,22 +3,13 @@ using namespace std;
 #include<stdlib.h>
 
 void GetParameter(int&,int&);
-int ** GetData(int,int);
+void GetData(int**,int,int);
 int main(){
-    int** M1;
+    int** M1,** M2;
     int d1,d2;
     GetParameter(d1,d2);
-    // M1=new int*[d1];
-    // for(int i=0;i<d1;i++){
-    //     M1[i]=new int[d2];
-    // }
-    M1 = GetData(d1,d2);
-    for(int j=0;j<d1;j++){
-        for(int k=0;k<d2;k++){
-            cout<<M1[j][k]<<" ";
-        }
-        cout<<endl;
-    }
+    GetData(M1,d1,d2);
+    cout<<M1[0][1];
     return 0;
 
 }
@@ -31,8 +22,8 @@ void GetParameter(int &d1,int &d2){
     d1=x;
     d2=y;
 }
-int** GetData(int d1,int d2){
-    int **arr;
+void GetData(int **arr,int d1,int d2){
+    //int ** arr;
     arr=new int*[d1];
     for(int i=0;i<d1;i++){
         arr[i]=new int[d2];
@@ -42,12 +33,4 @@ int** GetData(int d1,int d2){
             arr[j][k]=rand()%4;
         }
     }
-    for(int j=0;j<d1;j++){
-        for(int k=0;k<d2;k++){
-            cout<<arr[j][k]<<" ";
-        }
-        cout<<endl;
-    }
-    cout<<endl<<endl;
-    return arr;
 }
